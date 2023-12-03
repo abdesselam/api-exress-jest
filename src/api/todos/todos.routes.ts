faefd2585d595166405e71c870294b3dc74f4c19
abdesselam <abdesselam.mihoubi@gmail.com>
2023-12-03 23:30:17 +0100

    add route createOne

--- src/api/todos/todos.routes.ts
+++ src/api/todos/todos.routes.ts
@@ -4,6 +4,7 @@ import * as TodoHandlers from './todos.handler'
 
 const router = Router();
 
-router.get('/',TodoHandlers.findAll)
+router.get('/',TodoHandlers.findAll);
+router.post('/',TodoHandlers.createOne);
 
 export  default router
