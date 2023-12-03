87ace2720a10aec41f75031307afe6132fd99f5c
abdou <abdou@gmail.com>
2023-12-03 21:32:57 +0100

    add try-catch in in handler todo

--- src/api/todos/todos.handler.ts
+++ src/api/todos/todos.handler.ts
@@ -1,8 +1,13 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import {TodoWithId, Todos} from "./todos.model";
 
-export async function findAll(req: Request,res: Response<TodoWithId[]>) {
-    const resulte = await Todos.find();
-    const todos = await resulte.toArray();
-    res.json(todos);
+export async function findAll(req: Request,res: Response<TodoWithId[]>, next: NextFunction) {
+    try {
+        const resulte = await Todos.find();
+        const todos = await resulte.toArray();
+        res.json(todos);
+    } catch (error) {
+        next(error);
+    }
+    
 }
