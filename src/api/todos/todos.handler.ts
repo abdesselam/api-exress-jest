6defa02b9a5823651a1d6d4dbc3df9a4fe252158
abdesselam <abdesselam.mihoubi@gmail.com>
2023-12-03 23:14:56 +0100

    add function creatOne post

--- src/api/todos/todos.handler.ts
+++ src/api/todos/todos.handler.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from "express";
-import {TodoWithId, Todos} from "./todos.model";
+import {Todo,TodoWithId, Todos} from "./todos.model";
 
 export async function findAll(req: Request,res: Response<TodoWithId[]>, next: NextFunction) {
     try {
         const resulte = await Todos.find();
         const todos = await resulte.toArray();
         res.json(todos);
+    } catch (error) {
+        next(error);
+    }
+    
+}
+
+export async function createOne(req: Request<{},TodoWithId,Todo>,res: Response<TodoWithId>, next: NextFunction) {
+    try {
+        const validateResult = await Todo.parse(req.body)
     } catch (error) {
         next(error);
     }
