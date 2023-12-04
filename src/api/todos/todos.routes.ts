47ab53b78bcf8534a339dee5ad4b41196907509f
abdou <abdou@gmail.com>
2023-12-04 14:21:00 +0100

    refactotor middleware function

--- src/api/todos/todos.routes.ts
+++ src/api/todos/todos.routes.ts
@@ -1,40 +1,18 @@
-import { Router,Request,Response,NextFunction} from "express";
+import { Router} from "express";
 
 import * as TodoHandlers from './todos.handler'
-import { Todo, TodoWithId } from "./todos.model";
-import { AnyZodObject, ZodError } from "zod";
+import { Todo} from "./todos.model";
+import { validateRequest } from "../../middlewares";
 
 const router = Router();
 
-interface RequestValidators {
-    params?: AnyZodObject,
-    body?: AnyZodObject,
-    query?: AnyZodObject,
-}
-
-function validateRequest(validators: RequestValidators){
-return async(req: Request, res: Response ,next: NextFunction)=>{
-    try {
-        if (validators.params){
-            req.params = await validators.params.parseAsync(req.params);
-        }
-        if (validators.body){
-            req.body = await validators.body.parseAsync(req.body);
-        }
-        if (validators.query){
-            req.query = await validators.query.parseAsync(req.query);
-        }
-        next();
-    } catch (error) {
-        if (error instanceof ZodError){
-            res.status(422);
-    }
-        next(error)
-    }
-}
-}
 
 router.get('/',TodoHandlers.findAll);
-router.post('/',TodoHandlers.createOne);
+router.post('/',
+validateRequest({
+    body: Todo,
+}),
+TodoHandlers.createOne,
+);
 
 export  default router
