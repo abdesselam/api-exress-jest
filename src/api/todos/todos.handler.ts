dc17df21dbccae0ac007cf2ec89811f543366beb
abdesselam <abdesselam.mihoubi@gmail.com>
2023-12-04 15:09:56 +0100

    add findOne in handler

--- src/api/todos/todos.handler.ts
+++ src/api/todos/todos.handler.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import {Todo,TodoWithId, Todos} from "./todos.model";
 import {InsertOneResult} from 'mongodb'
 import { ZodError } from "zod";
+import {ObjectId} from 'mongodb'
+import { ParamsWithId } from "../../interfaces/ParamsWithId";
 
 export async function findAll(req: Request,res: Response<TodoWithId[]>, next: NextFunction) {
     try {
@@ -26,4 +28,19 @@ export async function createOne(req: Request<{},TodoWithId,Todo>,res: Response<T
         next(error);
     }
     
+}
+
+export async function findOne(req: Request<ParamsWithId,TodoWithId,{}>,res: Response<TodoWithId>, next: NextFunction) {
+    try {
+        const result = await Todos.findOne({
+            _id: new ObjectId(req.params.id)
+        })
+        if(!result){
+            res.status(404);
+            throw new Error(`Todo with id ${req.params.id} not found.`)
+        }
+        res.json(result);
+    } catch (error) {
+        next(error)
+    }
 }
